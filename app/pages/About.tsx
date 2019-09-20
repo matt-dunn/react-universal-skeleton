cd4e19ffb9d38a14f3feddf1dfe4e949f1991d26
Matt Dunn <mattdunn@Matts-MBP.home>
2019-09-20 16:01:59 +0100

    Update about component

--- app/pages/About.tsx
+++ app/pages/About.tsx
@@ -2,25 +2,55 @@ import React from 'react'
 import {Helmet} from 'react-helmet-async'
 
 import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
 import Page from '../components/Page.jsx'
 import styled from "styled-components";
 
-const Load = styled.div`
+import {IAppState} from '../reducers';
+import { IExampleResponse } from '../components/api/__dummy__/example';
+
+import * as actions from '../actions';
+
+import {IExampleItemState, IExampleListState} from '../reducers/__dummy__/example';
+// import {IExampleAction} from '../container';
+import { IStatus } from '../components/state-mutate-with-status/status';
+
+const Title = styled.div`
     color: blue;
     font-size: 50px;
 `
 
-const About = () => (
-    <Page>
-        <Helmet>
-            <title>About Page</title>
-        </Helmet>
-        <Load>
-            This is the about page!!!
-        </Load>
-    </Page>
-)
+export type IInjectedAboutProps = {
+    items: IExampleListState;
+    item: IExampleItemState;
+    onExampleAction: IExampleAction;
+    onExampleListAction: IExampleAction;
+    $status: IStatus;
+};
+
+export type IAboutProps = { testString: string; testNumber: number; } & IInjectedAboutProps;
+
+const About = ({items, item}: IAboutProps) => {
+    // console.error("ABOUT", items.toJS(), item.toJS())
+    return (
+        <Page>
+            <Helmet>
+                <title>About Page</title>
+            </Helmet>
+            <Title>
+                This is the about page
+            </Title>
+            <ul>
+                {items.map(item => (
+                    <li key={item.get("id")}>
+                        {item.get("name")}
+                    </li>
+                ))}
+            </ul>
+        </Page>
+    )
+}
 
 export type IExampleAction = (id: string, name: string) => Promise<IExampleResponse>;
 
