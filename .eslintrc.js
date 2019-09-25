1a37a4d5dc964bf61abcc9e77de9aa49d12189ac
Matt Dunn <mattdunn@Matts-MBP.home>
2019-09-25 15:33:08 +0100

    Configure eslint (with ts)

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports =  {
+    parser:  '@typescript-eslint/parser',
+    extends:  [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions:  {
+        ecmaVersion:  2018,
+        sourceType:  'module',
+        ecmaFeatures:  {
+            jsx:  true,
+        },
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        },
+        // "import/resolver": {
+        //     "node": {
+        //         "extensions": [
+        //             ".js",
+        //             ".jsx",
+        //             ".tsx",
+        //             ".ts"
+        //         ]
+        //     }
+        // }
+    },
+    rules:  {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+};
