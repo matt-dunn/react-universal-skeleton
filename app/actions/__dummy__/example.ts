bda176ca279dd3f2f762cbead6c610a1a937160b
Matt Dunn <mattdunn@Matts-MBP.home>
2019-10-01 12:17:38 +0100

    Code tidy

--- app/actions/__dummy__/example.ts
+++ app/actions/__dummy__/example.ts
@@ -1,9 +1,6 @@
 import { createStandardAction } from 'typesafe-actions';
 
-// import {IExampleList, IExampleResponse} from '../../components/api/__dummy__/example';
 import { IPayload } from '../../components/redux/middleware/stateDecorator';
-
-// import { IRootService } from 'web/config/service.config';
 
 const exampleGetList = createStandardAction('@__dummy__/EXAMPLE_GET_LIST')
     .map(() => ({
@@ -14,22 +11,5 @@ const exampleGetItem = createStandardAction('@__dummy__/EXAMPLE_GET_ITEM')
     .map(() => ({
         payload: ({ inject }: IPayload) => inject.services.exampleGetItem()
     }));
-
-// const example = createStandardAction('@__dummy__/EXAMPLE')
-//     .map(({ id, name }: IExampleResponse) => ({
-//         payload: ({ inject }: IPayload) => inject('service')((service: any) => service.exampleApi.example(id, name)),
-//         meta: { id, seedPayload: { id, name } },
-//     }));
-//
-// type Notification = {
-//     username: string;
-//     message?: string
-// };
-//
-// const exampleList = createStandardAction('@__dummy__/EXAMPLE_LIST')
-//     .map(({ id, name }: IExampleResponse, meta: Notification) => ({
-//         payload: ({ inject }: IPayload) => inject('service')((service: any) => service.exampleApi.exampleList(id, name)),
-//         meta: { id, ...meta },
-//     }));
 
 export { exampleGetList, exampleGetItem };
