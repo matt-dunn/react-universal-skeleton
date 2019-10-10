ab23e33a5338be4bf980b8984fa2e28dd49b271e
Matt Dunn <matt.j.dunn@gmail.com>
2019-10-10 14:38:28 +0100

    Refactor

--- components/state-mutate-with-status/index.ts
+++ components/state-mutate-with-status/index.ts
@@ -4,9 +4,9 @@ import { FluxStandardAction } from 'flux-standard-action';
 
 import { IStatusTransaction } from './status';
 
-import {getPendingState, setPendingState} from "./pendingTransactionState";
+import {setPendingState} from "./pendingTransactionState";
 
-import { decorateStatus } from './utils';
+import {decorateStatus, getPayload, getUpdatedState} from './utils';
 
 export type Path = ReadonlyArray<string>;
 
@@ -24,69 +24,6 @@ export interface Options<P> {
   path?: Path;
   getNewItemIndex?: GetNewItemIndex<P>;
 }
-
-type UpdatedStatus<S, P> = {
-  updatedState: S;
-  originalState?: P | null;
-}
-
-const getPayload = <S extends IStatusTransaction, P>(status: S, payload: P, seedPayload?: P): P | undefined | null => {
-  if (status.isActive) {
-    return status.hasError ? seedPayload : payload || seedPayload
-  } else if (status.hasError) {
-    return getPendingState(status.transactionId);
-  }
-
-  return status.complete ? payload : seedPayload
-};
-
-const getUpdatedState = <S, P, U extends IStatusTransaction>(state: S, payload: P, status: U, path: Path, actionId?: string, options?: Options<P>): UpdatedStatus<S, P> => {
-  if (actionId) {
-    const array = get(state, path);
-
-    if (Array.isArray(array)) {
-      const index = array.findIndex(item => item.id === actionId);
-
-      if (index === -1) {
-        if (payload) {
-          const { getNewItemIndex } = options || {} as Options<P>;
-
-          return {
-            updatedState: immutable.insert(state, path, Object.assign({}, payload, {$status: decorateStatus(status)}), getNewItemIndex ? getNewItemIndex(array, payload) : array.length),
-            originalState: null // Ensure final payload is not set so this item can be removed from the array on failure
-          }
-        }
-      } else if (payload === null) {
-        return {
-          updatedState: immutable.del(
-              state,
-              [...path, index.toString()]
-          ) as any
-        };
-      } else {
-        return {
-          updatedState: immutable.update(
-              (payload && immutable.assign(state, [...path, index.toString()], payload as any)) || state,
-              [...path, index.toString(), '$status'],
-              state => decorateStatus(status, state && state.$status)
-          ) as any,
-          originalState: get(state, [...path, index.toString()])
-        };
-      }
-    } else {
-      throw new TypeError(`Item in state at ${path.join('.')} must be an array when meta.id (${actionId}) is specified`);
-    }
-
-    return {
-      updatedState: state
-    };
-  } else {
-    return {
-      updatedState: (payload && immutable.assign(state, path, payload as any)) || state,
-      originalState: get(state, path)
-    };
-  }
-};
 
 const updateState = <S, P>(state: S, { meta, error, payload }: FluxStandardAction<string, P, ActionMeta>, options?: Options<P>): S => {
   const { path = [] } = options || {} as Options<P>;
--- components/state-mutate-with-status/utils.ts
+++ components/state-mutate-with-status/utils.ts
@@ -1,6 +1,15 @@
 import isEqual from 'lodash/isEqual';
+import { get } from 'lodash';
+import immutable from "object-path-immutable";
 
 import Status, {IStatus, IStatusTransaction, symbolActiveTransactions} from "./status";
+import {getPendingState} from "./pendingTransactionState";
+import {Options, Path} from "./index";
+
+export type UpdatedStatus<S, P> = {
+    updatedState: S;
+    originalState?: P | null;
+}
 
 export const decorateStatus = (status: IStatusTransaction, $status: IStatus = {} as IStatus): IStatus => {
     const activeTransactions = { ...$status[symbolActiveTransactions] };
@@ -65,3 +74,62 @@ export const deserialize = (s: string): any => {
         return value;
     })
 };
+
+export const getPayload = <S extends IStatusTransaction, P>(status: S, payload: P, seedPayload?: P): P | undefined | null => {
+    if (status.isActive) {
+        return status.hasError ? seedPayload : payload || seedPayload
+    } else if (status.hasError) {
+        return getPendingState(status.transactionId);
+    }
+
+    return status.complete ? payload : seedPayload
+};
+
+export const getUpdatedState = <S, P, U extends IStatusTransaction>(state: S, payload: P, status: U, path: Path, actionId?: string, options?: Options<P>): UpdatedStatus<S, P> => {
+    if (actionId) {
+        const array = get(state, path);
+
+        if (Array.isArray(array)) {
+            const index = array.findIndex(item => item.id === actionId);
+
+            if (index === -1) {
+                if (payload) {
+                    const { getNewItemIndex } = options || {} as Options<P>;
+
+                    return {
+                        updatedState: immutable.insert(state, path, Object.assign({}, payload, {$status: decorateStatus(status)}), getNewItemIndex ? getNewItemIndex(array, payload) : array.length),
+                        originalState: null // Ensure final payload is not set so this item can be removed from the array on failure
+                    }
+                }
+            } else if (payload === null) {
+                return {
+                    updatedState: immutable.del(
+                        state,
+                        [...path, index.toString()]
+                    ) as any
+                };
+            } else {
+                return {
+                    updatedState: immutable.update(
+                        (payload && immutable.assign(state, [...path, index.toString()], payload as any)) || state,
+                        [...path, index.toString(), '$status'],
+                        state => decorateStatus(status, state && state.$status)
+                    ) as any,
+                    originalState: get(state, [...path, index.toString()])
+                };
+            }
+        } else {
+            throw new TypeError(`Item in state at ${path.join('.')} must be an array when meta.id (${actionId}) is specified`);
+        }
+
+        return {
+            updatedState: state
+        };
+    } else {
+        return {
+            updatedState: (payload && immutable.assign(state, path, payload as any)) || state,
+            originalState: get(state, path)
+        };
+    }
+};
+
