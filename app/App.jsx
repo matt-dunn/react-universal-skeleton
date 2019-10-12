c5bad1819aea9f868396280090fbd7042d7a3745
Matt Dunn <matt.j.dunn@gmail.com>
2019-10-12 18:15:27 +0100

    Updates

--- app/App.jsx
+++ app/App.jsx
@@ -1,5 +1,5 @@
 import { hot } from 'react-hot-loader';
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import myStyled from "./myStyled";
 
@@ -13,33 +13,35 @@ const Fancy = ({children, className}) => {
 }
 
 const Styled = myStyled(Fancy)`
-    padding: ${({index}) => `${100 + ((index || 1) * 5)}px`};
+    padding: ${({index}) => `${20 + ((index || 0) * 50)}px`};
     color: ${({color}) => color};
-    border: 10px solid orange;
-    ${({color}) => color === "blue" && `
+    border: 3px solid orange;
+    ${({color, index}) => color === "blue" && index === 0 && `
         background-color: orange;
         font-weight: bold;
-    `};
+    `}
 `;
 
 const Styled2 = myStyled("div")`
     border: 1px solid red;
     padding: 10px;
-    color: yellow;
+    color: #ccc;
 `;
 
 const App = () => {
     const [color, setColor] = useState("green");
     const [index, setIndex] = useState(0);
 
-    setTimeout(() => {
-        setColor("blue")
-    }, 2000);
+    useEffect(() => {
+        setTimeout(() => {
+            setColor("blue")
+        }, 2000);
 
-    // setTimeout(() => {
-    //     // setColor("yellow")
-    //     setIndex(index + 1)
-    // }, 3000)
+        setTimeout(() => {
+            setColor("violet")
+            setIndex(index + 1)
+        }, 4000)
+    }, []);
 
     return (
         <Styled color={color} index={index}>
@@ -49,6 +51,6 @@ const App = () => {
             </Styled2>
         </Styled>
     );
-}
+};
 
 export default hot(module)(App);
