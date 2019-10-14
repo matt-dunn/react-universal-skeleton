538e4b81e57c2533678cddcc7efe56ca8329fa0f
Matt Dunn <matt.j.dunn@gmail.com>
2019-10-14 18:10:01 +0100

    Undo change to App

--- app/App.jsx
+++ app/App.jsx
@@ -1,77 +1,75 @@
 import { hot } from 'react-hot-loader';
-import React, {useEffect, useState} from 'react'
+import React from 'react'
+import {Helmet} from 'react-helmet-async';
+import {Switch, Route, generatePath} from 'react-router-dom'
+import importComponent from 'react-imported-component';
 
-import myStyled from "components/myStyled";
-// import myStyled from "styled-components";
+import Header from './components/Header'
 
-const Fancy = ({children, className}) => {
-    return (
-        <div className={className}>
-            Fancy:
-            {children}
-        </div>
-    );
+import Error403 from './pages/403';
+import Error404 from './pages/404';
+
+import Home from './pages/Home';
+import LoadingComponent from './pages/Loading'
+import ErrorComponent from './pages/Error'
+
+import { GlobalStyles } from './styles'
+import { ToastifyStyles } from './styles/toasify'
+
+import ErrorHandler from "components/actions/ErrorHandler";
+
+const About = importComponent(() => import("./pages/About"), {
+    LoadingComponent,
+    ErrorComponent
+});
+
+const Login = importComponent(() => import("./pages/Login"), {
+    LoadingComponent,
+    ErrorComponent
+});
+
+const handler = ({code, status}, location, history, props) => {
+    console.log("HANDLE ERROR", code, status, location, history)
+
+    if (status === 401) {
+        history.push(generatePath("/login/:from?", {from: location}))
+        return true;
+    } else if (status === 403) {
+        return <Error403/>;
+    } else if (status === 404) {
+        return <Error404/>;
+    }
 }
 
-const Styled = myStyled(Fancy)`
-    padding: ${({index}) => `${20 + ((index || 0) * 50)}px`};
-    color: ${({color}) => color};
-    border: 3px solid orange;
-    ${({color, index}) => color === "blue" && index === 0 && `
-        background-color: orange;
-        font-weight: bold;
-    `}
+import AuthProvider from "./components/auth"
 
-    &:hover {
-        background-color: ${({color}) => (color === "blue" && "green") || "yellow"}
-    }
-    
-    address {
-        margin: ${({index}) => `${20 + ((index || 0) * 50)}px`};
-    }
-`;
-
-const Styled2 = myStyled.address`
-    border: 1px solid red;
-    padding: 10px;
-
-    &:hover {
-        background-color: red;
-    }
-`;
-
-const Styled3 = myStyled(Styled2)`
-    font-size: 20px;
-`;
-
-const v = "bold"
-const Styled4 = myStyled(Styled3)`
-    font-weight: ${v};
-`;
+// Parcel seems to need the node_modules path prefixed otherwise server bundle get Unexpected Token error... :(
+import '/node_modules/react-responsive-ui/style.css';
 
 const App = () => {
-    const [color, setColor] = useState("green");
-    const [index, setIndex] = useState(0);
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setColor("blue")
-    //     }, 2000);
-    //
-    //     setTimeout(() => {
-    //         setColor("violet")
-    //         setIndex(index => index + 1)
-    //     }, 4000)
-    // }, []);
-
     return (
-        <Styled color={color} index={index}>
-            {color}
-            <Styled4 className="my-lovely-horse">
-                INNER: {color}
-            </Styled4>
-        </Styled>
+        <AuthProvider>
+            <GlobalStyles/>
+            <ToastifyStyles/>
+
+            <Helmet>
+                <title>My App</title>
+                <meta name="build.version" content={process.env.npm_package_version + ((process.env.NODE_ENV !== "production" && "-dev") || "")}/>
+            </Helmet>
+
+            <Header/>
+
+            <ErrorHandler handler={handler}>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about/:page?" render={() => <About />} />
+                    <Route path="/login/:from?" render={() => <Login />} />
+                    <Route component={Error404} />
+                    {/*<Redirect to="/" />*/}
+                </Switch>
+            </ErrorHandler>
+        </AuthProvider>
     );
-};
+}
 
 export default hot(module)(App);
