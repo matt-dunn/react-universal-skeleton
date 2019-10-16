0078bce694ab28deb3069daef01711b7338b2689
Matt Dunn <matt.j.dunn@gmail.com>
2019-10-16 17:25:09 +0100

    Stylesheet typing

--- components/myStyled/stylesheet.ts
+++ components/myStyled/stylesheet.ts
@@ -1,40 +1,58 @@
-import React from 'react'
+import React, {ComponentType} from 'react'
 import {StyleContext} from "./index";
 
-const Rule = cssText => {
+export type CSSRule = {
+    cssText: string;
+    cssRules: CSSRule[];
+}
+
+export type ParentRule = {
+    cssText: string;
+    selectorText: string;
+}
+
+const Rule = (cssText: string): CSSRule | ParentRule => {
     if (cssText.substr(0, 1) === "@") {
         const match = cssText.match(/.*?\{(?<selectorText>.*)/);
 
         return {
             cssText,
-            cssRules: match && match.groups.selectorText.slice(0, -1).split("}").map(rule => rule && Rule(rule + "}")).filter(rule => rule)
-        }
+            cssRules: match && match.groups && match.groups.selectorText.slice(0, -1).split("}").map(rule => rule && Rule(rule + "}")).filter(rule => rule)
+        } as CSSRule
     } else {
         const match = cssText.match(/(?<selectorText>.*?)\{/);
 
-        const selectorText = match && match.groups.selectorText.trim();
+        const selectorText = match && match.groups && match.groups.selectorText.trim();
 
         return {
             cssText,
             selectorText
-        };
+        } as ParentRule;
     }
 };
 
-const Stylesheet = () => {
-    const rules = [];
-    const hashes = [];
+export interface Stylesheet {
+    rules: (CSSRule | ParentRule)[];
+    hashes: string[];
+    deleteRule: (index: number) => void;
+    insertRule: (cssText: string, index?: number) => number;
+    collectHash: (hash: string) => number;
+}
 
-    const deleteRule = index => {
+const Stylesheet = (): Stylesheet => {
+    const rules: (CSSRule | ParentRule)[] = [];
+    const hashes: string[] = [];
+
+    const deleteRule = (index: number) => {
         rules.splice(index, 1);
     };
 
-    const insertRule = (cssText, index) => {
+    const insertRule = (cssText: string, index?: number) => {
         rules.splice(index || rules.length - 1, 0, Rule(cssText));
         return index || rules.length - 1;
     };
 
-    const collectHash = hash => hashes.push(hash);
+    const collectHash = (hash: string) => hashes.push(hash);
 
     return {
         rules,
@@ -48,7 +66,7 @@ const Stylesheet = () => {
 export default () => {
     const stylesheet = Stylesheet();
 
-    const collectStyles = app => {
+    const collectStyles = (app: ComponentType) => {
         return React.createElement(
             StyleContext.Provider,
             {value: stylesheet},
