8b0c8142b3413f5b154ae549b36316826302eec8
Matt Dunn <matt.j.dunn@gmail.com>
2019-10-19 17:06:32 +0100

    Make autosave action optional

--- components/hooks/useAutosave.ts
+++ components/hooks/useAutosave.ts
@@ -10,53 +10,47 @@ export type Action<T> = {
     (...args: any[]): Promise<T>;
 }
 
-function useAutosave<T = any>(action?: Action<T>, options?: Options): Action<T> {
+function useAutosave<T = any>(action?: Action<T>, options?: Options): Action<T> | undefined {
     const timeout = useRef<number>();
     const isActive = useRef<boolean>(false);
     const pending = useRef<any>();
 
     const {delay = 500, onSaving, onComplete} = options || {};
 
-    return useCallback(
-        (...args): any => {
-            if (!action) {
-                return Promise.resolve();
-            }
+    return action && useCallback(
+        (...args): any => new Promise<T>((resolve, reject) => {
+            if (!isActive.current) {
+                clearTimeout(timeout.current);
 
-            return new Promise<T>((resolve, reject) => {
-                if (!isActive.current) {
-                    clearTimeout(timeout.current);
+                timeout.current = setTimeout(() => {
+                    const exec = (...args: any[]) => {
+                        isActive.current = true;
+                        onSaving && onSaving(...args);
 
-                    timeout.current = setTimeout(() => {
-                        const exec = (...args: any[]) => {
-                            isActive.current = true;
-                            onSaving && onSaving(...args);
-
-                            action(...args)
-                                .then(payload => {
-                                    if (pending.current) {
-                                        exec(...pending.current)
-                                        pending.current = undefined;
-                                    } else {
-                                        isActive.current = false;
-                                        onComplete && onComplete(...args);
-                                        resolve(payload)
-                                    }
-                                })
-                                .catch(reason => {
+                        action(...args)
+                            .then(payload => {
+                                if (pending.current) {
+                                    exec(...pending.current)
                                     pending.current = undefined;
+                                } else {
                                     isActive.current = false;
-                                    reject(reason);
-                                });
-                        };
+                                    onComplete && onComplete(...args);
+                                    resolve(payload)
+                                }
+                            })
+                            .catch(reason => {
+                                pending.current = undefined;
+                                isActive.current = false;
+                                reject(reason);
+                            });
+                    };
 
-                        exec(...args);
-                    }, delay)
-                } else {
-                    pending.current = args;
-                }
-            })
-        },
+                    exec(...args);
+                }, delay)
+            } else {
+                pending.current = args;
+            }
+        }),
         [action, delay, onComplete, onSaving]
     )
 }
