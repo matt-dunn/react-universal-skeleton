5f54c38def82d28baa5a992c084c5d5ba099a063
Matt Dunn <matt.j.dunn@gmail.com>
2019-10-25 18:30:38 +0100

    Update Form

--- app/components/Form/index.tsx
+++ app/components/Form/index.tsx
@@ -1,17 +1,23 @@
-import React, {useCallback, useContext, useState, useEffect, ReactEventHandler, FormEvent} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import styled, {css} from "styled-components";
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import useWhatChanged from "components/whatChanged/useWhatChanged";
 
-import {FormDataContext, useFormData, FormData} from "components/Form";
+import {useFormData} from "components/Form";
 import ReactSelect from "react-select";
-import useEffectAction from "components/actions/useEffectAction";
 import {errorLike} from "components/error";
+import {APIContext} from "components/actions/contexts";
+
 
 const Form = styled.form`
-  display: flex;
-  width: 100%;
-  font-size: 16px;
+  border: 1px solid #ccc;
+  background-color: #fdfdfd;
+  border-radius: 4px;
+  padding: 10px;
+  margin: 20px 0;
 `;
 
 const SelectStyle = css`
@@ -33,7 +39,30 @@ const Button = styled.button`
   border: 1px solid #ccc;
   background-color: #eee;
   border-radius: 3px;
-  margin-left: 8px;
+  margin: 10px 8px 10px 0;
+`
+
+const Label = styled.label`
+  color: #666;
+  margin: 2px 0;
+`
+
+const Input = styled.input`
+  && {
+  font-size: inherit;
+  border: 1px solid rgb(204, 204, 204);
+  padding: 9px 8px;
+  border-radius: 4px;
+  }
+`
+
+const InputFeedback = styled.div`
+  color: red;
+  margin: 5px 0 10px 0;
+`
+
+const Section = styled.div`
+  margin: 0 0 10px 0;
 `
 
 const options = [
@@ -48,29 +77,46 @@ type Option = {
     label: string;
 }
 
-const FancySelect = ({options, name, value, onChange}: {options: Option[]; name: string; value?: string; onChange: (value: Option) => void;}) => {
+const FancySelect = ({id, options, name, value, onChange, onBlur}: {id: string; options: Option[]; name: string; value?: string; onChange: (name: string, value: string) => void; onBlur: (name: string, touched: boolean) => void;}) => {
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
         setIsClient(true)
     }, [])
 
+    const handleChange = ({value}: {value: string;}) => {
+        // this is going to call setFieldValue and manually update values.topcis
+        onChange(name, value);
+    };
+
+    const handleBlur = () => {
+        onBlur(name, true);
+    };
+
+    const handleSelectChange = () => {
+
+    }
+
     if (isClient) {
         const defaultValue = (value && options.filter(option => option.value ===value)[0]) || options[0];
         return (
             <Select
+                id={id}
                 name={name}
-                defaultValue={defaultValue}
+                value={defaultValue}
                 options={options}
-                onChange={onChange}
+                onChange={handleChange}
+                onBlur={handleBlur}
             />
         )
     } else {
         return (
             <BasicSelect
+                id={id}
                 className="no-js"
                 name={name}
                 value={value}
+                onChange={handleSelectChange}
             >
                 {options.map((option, index) => (
                     <option key={index} value={option.value}>{option.label}</option>
@@ -82,95 +128,170 @@ const FancySelect = ({options, name, value, onChange}: {options: Option[]; name:
 
 type MyForm = {
     flavour: string;
+    email: string;
 }
 
 type MyFormResponse = {
     chosenFlavour: string;
+    yourEmail: string;
 }
 
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email()
+        .required('Email is required'),
+    flavour: Yup.string()
+        .required('Flavour is required')
+});
 
 const MyForm = () => {
-    const data = useFormData<MyForm, MyFormResponse>();
+    const formDataContext = useFormData<MyForm, MyFormResponse>();
+    const [formData, setFormData] = useState(formDataContext);
 
-    const [formData, setFormData] = useState(data);
+    // console.log("@@@@@", formData, formData.payload)
 
-    const {flavour} = formData.data;
+    // const x = React.createRef<Formik>()
 
-    const isValid = Boolean(formData.isSubmitted && flavour);
+    const context = useContext<Promise<any>[] | undefined>(APIContext as any);
 
-    console.log("@@@@@", formData, formData.payload)
-
-    const submit = (formData: FormData) => {
-        console.log("@@@@@: CALL API", formData)
-        formData.isProcessed = true;
-        return new Promise<MyFormResponse>(resolve => {
-            if (flavour === "vanilla") {
+    const submit = (data: MyForm): Promise<MyFormResponse> => {
+        console.log("@@@@@@CALL FORM API")
+        return new Promise((resolve, reject) => {
+            if (data.flavour === "vanilla") {
                 // throw new APIError("Authentication Failed", "auth", 403)
-                throw new Error("Bumhole!")
+                throw new Error("Don't like VANILLA!!!")
             }
+
             setTimeout(() => {
-                resolve({chosenFlavour: `CHOSEN: ${flavour}`});
+                resolve({chosenFlavour: `FLAVOUR: ${data.flavour}`, yourEmail: `EMAIL: ${data.email}`})
             }, 2000);
+
         })
-            .then(payload => {
-                formData.payload = payload
-                return payload
-            })
-            .catch(ex => {
-                formData.error = errorLike(ex);
-                throw ex;
-            })
     }
 
-    const handleSubmit = (e: FormEvent) => {
-        console.log(">>>", formData)
-
-        setFormData(formData => ({...formData, isSubmitted: true, isProcessed: true, error: undefined}))
-
-        submit(formData)
-            .then((payload: any) => {
-                console.log("END", payload)
-                setFormData(formData => ({...formData, payload}))
+    if (context && formDataContext.isSubmitted && !formDataContext.isProcessed) {
+        context.push(schema.validate(formDataContext.data, {abortEarly: false})
+            .then(data => {
+                return submit(data)
+                    .then(payload => {
+                        formDataContext.payload = payload;
+                    })
+                    .catch(reason => {
+                        formDataContext.error = errorLike(reason);
+                        throw reason;
+                    })
             })
-            .catch(error => {
-                setFormData(formData => ({...formData, isSubmitted: false, isProcessed: false, error}))
+            .catch(reason => {
+                console.log("ERROR@@@@", reason)
+
+                if (reason.inner) {
+                    formDataContext.errors = reason.inner.reduce((errors: any, error: any) => {
+                        errors[error.path] = error.message;
+                        return errors;
+                    }, {})
+                }
             })
-
-        e.preventDefault();
+            .finally(() => {
+                formDataContext.isProcessed = true;
+            })
+        );
     }
-
-    const handleChange = ({value}: Option) => {
-        console.log(">>>!!", value)
-        setFormData(formData => ({...formData, isSubmitted: false, isProcessed: false, error: undefined, data: {...formData.data, flavour: value}}))
-    }
-
-    useEffectAction(
-        () => submit(formData),
-        () => formData.isSubmitted && !formData.isProcessed && isValid
-    );
 
     // useWhatChanged(About, { formData, items, item, onExampleGetList, onExampleGetItem, onExampleEditItem, $status, renderListItem, page});
 
     return (
         <div>
-            {formData.payload && <p>{formData.payload.chosenFlavour}</p>}
-            {(isValid && !formData.error) && <p>You submitted '{flavour}'</p>}
-            {formData.error && <p style={{color: "red"}}>There was a problem submitting</p>}
-            {(formData.isSubmitted && !isValid) && <p style={{color: "red"}}>Flavour is required</p>}
-            <Form method="post" action="">
-                <FancySelect
-                    options={options}
-                    name="flavour"
-                    value={formData.data["flavour"]}
-                    onChange={handleChange}
-                />
-                <Button
-                    type="submit"
-                    onClick={handleSubmit}
-                >
-                    Go
-                </Button>
-            </Form>
+            {formData.payload && <pre>{JSON.stringify(formData.payload)}</pre>}
+
+            {formData.error && <InputFeedback>There was a problem submitting: {formData.error.message}</InputFeedback>}
+
+            <Formik
+                initialValues={formData.data || { email: '', flavour: '' }}
+                onSubmit={(values, { setSubmitting }) => {
+                    submit(values as any)
+                        .then(payload => {
+                            setFormData(formData => ({...formData, isProcessed: true, error: undefined, payload: payload, data: values as any}))
+                        })
+                        .catch(reason => {
+                            setFormData(formData => ({...formData, isProcessed: true, error: reason, payload: undefined, data: values as any}))
+                        })
+                        .finally(() => {
+                            setSubmitting(false);
+                        })
+                }}
+                validationSchema={schema}
+            >
+                {props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        dirty,
+                        isSubmitting,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        handleReset,
+                        setFieldTouched,
+                        setFieldValue
+                    } = props;
+                    return (
+                        <Form onSubmit={handleSubmit} method="post">
+                            <Section>
+                                <Label htmlFor="flavour" style={{ display: 'block' }}>
+                                    Flavour
+                                </Label>
+                                <FancySelect
+                                    id="flavour"
+                                    options={options}
+                                    name="flavour"
+                                    value={values.flavour}
+                                    onBlur={setFieldTouched}
+                                    onChange={setFieldValue}
+                                />
+                                {((formData.errors && formData.errors.flavour && !touched.flavour) || (errors.flavour && touched.flavour)) && (
+                                    <InputFeedback>{errors.flavour || formData.errors.flavour}</InputFeedback>
+                                )}
+                            </Section>
+
+                            <Section>
+                                <Label htmlFor="email" style={{ display: 'block' }}>
+                                    Email
+                                </Label>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    placeholder="Enter your email"
+                                    type="text"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    className={
+                                        errors.email && touched.email ? 'text-input error' : 'text-input'
+                                    }
+                                />
+                                {((formData.errors && formData.errors.email && !touched.email) || (errors.email && touched.email)) && (
+                                    <InputFeedback>{errors.email || formData.errors.email}</InputFeedback>
+                                )}
+                            </Section>
+
+                            <p>
+                                <Button
+                                    type="button"
+                                    className="outline"
+                                    onClick={handleReset}
+                                    disabled={!dirty || isSubmitting}
+                                >
+                                    Reset
+                                </Button>
+                                <Button type="submit" disabled={isSubmitting}>
+                                    Submit
+                                </Button>
+                            </p>
+                        </Form>
+                    );
+                }}
+            </Formik>
         </div>
     )
 }
--- components/Form/index.ts
+++ components/Form/index.ts
@@ -2,23 +2,29 @@ import React, {useContext} from "react";
 import {isEmpty} from "lodash";
 import {errorLike, ErrorLike} from "components/error";
 
-export type FormData<T = {}, P = {}> = {
+export type Errors<T> = {
+    [Key in keyof T]: string;
+}
+
+export type FormData<T = {}, P = any> = {
     isProcessed: boolean;
     data: T;
     isSubmitted: boolean;
     payload?: P;
     error?: ErrorLike;
+    errors: Errors<T>;
 };
 
-export const FormData = <T = {}>(formData: FormData<T>): FormData => {
-    const {isProcessed = false, data = {}, isSubmitted = !isEmpty(data), payload, error} = formData || {} as FormData;
+export const FormData = <T = {}, P = any>(formData: FormData<T, P>): FormData<T, P> => {
+    const {isProcessed = false, data = {} as T, payload, error, errors} = formData || {} as FormData<T, P>;
 
     return {
         isProcessed,
         data,
-        isSubmitted,
+        isSubmitted: !isEmpty(data),
         payload,
-        error: error && errorLike(error)
+        error: error && errorLike(error),
+        errors
     }
 };
 
@@ -26,7 +32,7 @@ export const FormDataContext = React.createContext<FormData | undefined>(undefin
 
 export const FormDataProvider = FormDataContext.Provider;
 
-export const useFormData = <T = {}, P = {}>(): FormData<T, P> => {
+export const useFormData = <T = {}, P = any>(): FormData<T, P> => {
     const formData = useContext(FormDataContext);
 
     return formData as FormData<T, P>
