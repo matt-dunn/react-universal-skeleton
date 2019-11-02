99a2a59120b94df354981bced0c0ce5201e234c7
Matt Dunn <matt.j.dunn@gmail.com>
2019-11-02 11:47:54 +0000

    Tidy

--- app/components/Form/index.tsx
+++ app/components/Form/index.tsx
@@ -1,32 +1,25 @@
-import React, {ComponentElement, ComponentType, useMemo} from 'react'
+import React, {ComponentType, useMemo} from 'react'
 import styled, {css} from "styled-components";
-
+import FormLabel from "app/components/Form/Label";
+import immutable from "object-path-immutable";
+import {string} from "yup";
+import {SchemaDescription} from "yup";
 import {
     Formik,
     ErrorMessage,
     Field,
-    useField,
-    FieldAttributes,
-    useFormikContext,
     setIn,
     getIn,
     FormikErrors,
     FormikTouched,
-    FieldArray, FormikHelpers
+    FieldArray
 } from 'formik';
 import * as Yup from 'yup';
-import {ValidationError, Schema, InferType, AnySchemaConstructor} from "yup";
+import {ValidationError, Schema} from "yup";
 
 import useWhatChanged from "components/whatChanged/useWhatChanged";
 
 import {MapDataToAction, useForm} from "components/actions/form";
-import FormLabel from "app/components/Form/Label";
-import FancySelect from "app/components/FancySelect";
-import immutable from "object-path-immutable";
-import {string} from "yup";
-import {ObjectSchemaDefinition} from "yup";
-import {Ref} from "yup";
-import {SchemaDescription} from "yup";
 
 const Form = styled.form`
   border: 1px solid #ccc;
@@ -87,190 +80,6 @@ const Legend = styled.legend`
   background-color: #eee;
   border-radius: 1em;
 `
-
-// const options = [
-//     { value: '', label: 'Select...' },
-//     { value: 'chocolate', label: 'Chocolate' },
-//     { value: 'strawberry', label: 'Strawberry' },
-//     { value: 'vanilla', label: 'Vanilla' },
-// ];
-//
-// type MyFormResponse = {
-//     chosenFlavour: string;
-//     yourEmail: string;
-// }
-
-// const validateEmailApi = (function() {
-//     let t: number;
-//
-//     return (email: string): Promise<boolean | ValidationError> => {
-//         console.log("#####VALIDATE EMAIL")
-//         clearTimeout(t);
-//         return new Promise((resolve, reject) => {
-//             if (email === "matt.j.dunn@gmail.com") {
-//                 throw new Error("Email validation failed")
-//             }
-//
-//             t = setTimeout(() => {
-//                 // reject(new Error("Email validation failed"))
-//                 resolve(!email.startsWith("demo@"))
-//             }, 1500)
-//         })
-//     }
-// })()
-
-// const schema = Yup.object().shape({
-//     email: Yup.string()
-//         .label("Email")
-//         .ensure()
-//         .meta({
-//             order: 1,
-//             Type: Input,
-//             props: {
-//                 placeholder: "Enter your email",
-//                 type: "text"
-//             }
-//         })
-//         .required('Email is required')
-//         .ensure()
-//         .email()
-//         .test("email", "Email ${value} is unavailable", function(value: string) {
-//             if (!value || !Yup.string().email().isValidSync(value)) {
-//                 return true;
-//             } else {
-//                 return validateEmailApi(value)
-//                     .catch(reason => new ValidationError(reason.message, value, this.path))
-//             }
-//         }),
-//     flavour: Yup.object()
-//         .meta({
-//             order: 0
-//         })
-//         .shape({
-//         favourite: Yup.string()
-//             .label("Flavour")
-//             .ensure()
-//             .meta({
-//                 order: 1,
-//                 Type: FancySelect,
-//                 props: {
-//                     options: [
-//                         { value: '', label: 'Select...' },
-//                         { value: 'chocolate', label: 'Chocolate' },
-//                         { value: 'strawberry', label: 'Strawberry' },
-//                         { value: 'vanilla', label: 'Vanilla' },
-//                     ]
-//                 }
-//             })
-//             .required('Flavour is required')
-//     }),
-//     notes: Yup.string()
-//         .required('Notes is required')
-//         .label("Notes")
-//         .ensure()
-//         .meta({
-//             order: 2,
-//             Type: Textarea,
-//             props: {
-//                 placeholder: "Enter your notes",
-//                 type: "text"
-//             }
-//         }),
-//     items: Yup.array(Yup.object()
-//         .shape({
-//             name: Yup.string()
-//                 .label("Name")
-//                 .meta({
-//                     order: 0,
-//                     Type: Input,
-//                     props: {
-//                         placeholder: "Enter your name",
-//                         type: "text"
-//                     }
-//                 })
-//                 .ensure()
-//                 .required(),
-//             address: Yup.string()
-//                 .label("Address")
-//                 .meta({
-//                     order: 1,
-//                     Type: Input,
-//                     props: {
-//                         placeholder: "Enter your address",
-//                         type: "text"
-//                     }
-//                 })
-//                 .ensure()
-//         }))
-//         .label("Peeps")
-//         .ensure()
-//         // .default([{name: "", address: ""}])
-//         .min(1)
-//         .max(5)
-// });
-
-// const dummyApiCall = (flavour: string, email: string): Promise<MyFormResponse> => {
-//     console.log("#####CALL API")
-//     return new Promise((resolve, reject) => {
-//         if (flavour === "vanilla") {
-//             // throw new APIError("Authentication Failed", "auth", 403)
-//             throw new Error("Don't like VANILLA!!!")
-//         }
-//
-//         setTimeout(() => {
-//             resolve({chosenFlavour: `FLAVOUR: ${flavour}`, yourEmail: `EMAIL: ${email}`})
-//         }, 2000);
-//     })
-// };
-
-// const MyField = ({ setFieldError, setStatus, status, ...props }: FieldAttributes<any> & {setFieldError: (field: string, value: string | undefined) => void; setStatus: (status: any) => void; status: any}) => {
-//     const {errors} = useFormikContext();
-//
-//     const fieldSchema:any = schema[props.name];
-//     const fieldSchemaAsync:any = schema[`${props.name}Async`];
-//
-//     console.log("#FIELD", props.name)
-//
-//     const validateField = (value: string) => {
-//         console.log("#VAL", value)
-//         return fieldSchema.validate(value)
-//             .then(() => {
-//                 console.log("#VAL=OK1", value)
-//                 if (fieldSchemaAsync) {
-//                     if (status && status.email && status.email.value === value) {
-//                         // return "";
-//                         return errors[props.name];
-//                     }
-//
-//                     setFieldError(props.name, "");
-//
-//                     return fieldSchemaAsync.validate(value, {context: {setFieldError, setStatus}})
-//                         .then(() => {
-//                             setFieldError(props.name, "");
-//                             return "";
-//                         })
-//                         .catch(reason => {
-//                             setFieldError(props.name, reason.message);
-//                             return reason.message;
-//                         })
-//                 }
-//                 return "";
-//             })
-//             .catch(reason => {
-//                 setFieldError(props.name, reason.message);
-//                 return reason.message;
-//             })
-//             .finally(() => {
-//                 setStatus({...status, email: {value: value}});
-//             })
-//     }
-//
-//     const {validate, ...fieldProps} = props
-//
-//     return (
-//         <Field {...fieldProps} />
-//     );
-// };
 
 type InitialFormData<T> = {
     errors: FormikErrors<T>;
@@ -335,7 +144,6 @@ function Fields<T extends object>({schema, fields, values, errors, touched, isSu
             {Object.keys(fields).sort((a, b) => ((fields[a]._meta || {}).order || 0) - ((fields[b]._meta || {}).order || 0)).map(key => {
                 const field = fields[key];
                 const fullPath = [path, key].filter(part => part).join(".");
-                // console.log(">>>", fullPath)
 
                 const {type, label, meta, tests} = field.describe();
                 const {Type, props} = field._meta || {};
@@ -353,11 +161,7 @@ function Fields<T extends object>({schema, fields, values, errors, touched, isSu
                         return o;
                     }, {min: 0, max: undefined})
 
-                    // console.log(">>!", field.describe(), value, fullPath, min, max, field.default())
-
                     const itemsCount = (value && value.length) || 0;
-
-                    console.log(field)
 
                     return (
                         <FieldArray
@@ -523,7 +327,7 @@ function MyForm<T, P>({schema, mapDataToAction}: FormProps<T, P>) {
 
     const initialValues: Yup.InferType<typeof schema> = formData.data || getDefault(schema);
 
-    console.log("@@@", initialValues && initialValues)
+    console.log("@@@", initialValues)
 
     useWhatChanged(MyForm, { formData, submit, initialValues });
 
@@ -545,93 +349,51 @@ function MyForm<T, P>({schema, mapDataToAction}: FormProps<T, P>) {
                         errors,
                         dirty,
                         isSubmitting,
-                        handleChange,
-                        handleBlur,
                         handleSubmit,
                         handleReset,
                         setFieldTouched,
                         setFieldValue,
-                        setFieldError,
-                        setStatus,
-                        status,
                         isValid,
                         isInitialValid
                     } = props;
-                    // console.log("####ERRORS", errors)
+
                     useWhatChanged(Formik, { formData, submit, props });
-                    return (
-                        <Form onSubmit={handleSubmit} method="post">
-                            {formData.error && <InputFeedback>There was a problem submitting: {formData.error.message}</InputFeedback>}
 
-                            {schema.fields && <Fields
-                                schema={schema}
-                                fields={schema.fields}
-                                values={values}
-                                errors={errors}
-                                touched={touched}
-                                isSubmitting={isSubmitting}
-                                setFieldValue={setFieldValue}
-                                setFieldTouched={setFieldTouched}
-                            />}
+                    if (schema.fields) {
+                        return (
+                            <Form onSubmit={handleSubmit} method="post">
+                                {formData.error &&
+                                <InputFeedback>There was a problem submitting: {formData.error.message}</InputFeedback>}
 
-                            {/*<Section>*/}
-                            {/*    <FormLabel label="Flavour" name="flavour.favourite" schema={schema}/>*/}
-                            {/*    <Field*/}
-                            {/*        as={FancySelect}*/}
-                            {/*        id="flavour"*/}
-                            {/*        options={options}*/}
-                            {/*        name="flavour.favourite"*/}
-                            {/*        value={values.flavour.favourite}*/}
-                            {/*        onBlur={setFieldTouched}*/}
-                            {/*        onChange={setFieldValue}*/}
-                            {/*        disabled={isSubmitting}*/}
-                            {/*        isValid={!(errors.flavour && errors.flavour.favourite && touched.flavour && touched.flavour.favourite)}*/}
-                            {/*        // status={status}*/}
-                            {/*        // setFieldError={setFieldError}*/}
-                            {/*        // setStatus={setStatus}*/}
-                            {/*    />*/}
-                            {/*    <ErrorMessage name="flavour.favourite">*/}
-                            {/*        {message => <InputFeedback>{message}</InputFeedback>}*/}
-                            {/*    </ErrorMessage>*/}
-                            {/*</Section>*/}
+                                {<Fields
+                                    schema={schema}
+                                    fields={schema.fields}
+                                    values={values}
+                                    errors={errors}
+                                    touched={touched}
+                                    isSubmitting={isSubmitting}
+                                    setFieldValue={setFieldValue}
+                                    setFieldTouched={setFieldTouched}
+                                />}
 
-                            {/*<Section>*/}
-                            {/*    <FormLabel label="Email" name="email" schema={schema}/>*/}
-                            {/*    <Field*/}
-                            {/*        as={Input}*/}
-                            {/*        id="email"*/}
-                            {/*        name="email"*/}
-                            {/*        placeholder="Enter your email"*/}
-                            {/*        type="text"*/}
-                            {/*        value={values.email}*/}
-                            {/*        onChange={handleChange}*/}
-                            {/*        onBlur={handleBlur}*/}
-                            {/*        disabled={isSubmitting}*/}
-                            {/*        isValid={!(errors.email && touched.email)}*/}
-                            {/*        // status={status}*/}
-                            {/*        // setFieldError={setFieldError}*/}
-                            {/*        // setStatus={setStatus}*/}
-                            {/*    />*/}
-                            {/*    <ErrorMessage name="email">*/}
-                            {/*        {message => <InputFeedback>{message}</InputFeedback>}*/}
-                            {/*    </ErrorMessage>*/}
-                            {/*</Section>*/}
+                                <p>
+                                    <Button
+                                        type="button"
+                                        className="outline"
+                                        onClick={handleReset}
+                                        disabled={!dirty || isSubmitting}
+                                    >
+                                        Reset
+                                    </Button>
+                                    <Button type="submit" disabled={isSubmitting} name="@@SUBMIT">
+                                        Submit {(isValid && isInitialValid) && "✔"}
+                                    </Button>
+                                </p>
+                            </Form>
+                        );
+                    }
 
-                            <p>
-                                <Button
-                                    type="button"
-                                    className="outline"
-                                    onClick={handleReset}
-                                    disabled={!dirty || isSubmitting}
-                                >
-                                    Reset
-                                </Button>
-                                <Button type="submit" disabled={isSubmitting} name="@@SUBMIT">
-                                    Submit {(isValid && isInitialValid) && "✔"}
-                                </Button>
-                            </p>
-                        </Form>
-                    );
+                    return null;
                 }}
             </Formik>
         </>
