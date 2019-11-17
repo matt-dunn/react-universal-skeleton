6ef266836eb9c7e729ea362757835edbbe174a20
Matt Dunn <matt.j.dunn@gmail.com>
2019-11-17 18:15:42 +0000

    Tidy SSR

--- server/lib/client.js
+++ server/lib/client.js
@@ -1,22 +1,11 @@
 import fs from 'fs';
 import path from 'path';
-import cheerio from 'cheerio';
 
 export const htmlPath = path.join(process.cwd(), 'dist', 'client', 'index.html');
 
 export const rawHTML = fs.readFileSync(htmlPath).toString();
 
-// export const parseRawHTMLForData = (template, selector = "#js-entrypoint") => {
-//     const $template = cheerio.load(template);
-//     let src = $template(selector).attr('src')
-//
-//     return {
-//         src
-//     }
-// }
-
-// const clientData = parseRawHTMLForData(rawHTML)
-
+const headString = "<head>"
 const appString = '<div id="app"\>'
 const splitter = '###SPLIT###'
 const [
@@ -25,13 +14,25 @@ const [
 ] = rawHTML
     .replace(appString, `${appString}${splitter}`)
     .split(splitter)
-
-// console.error(">>>>>",rawHTML)
-// console.error(">>>>>",startingRawHTMLFragment)
-// console.error(">>>>>",endingRawHTMLFragment)
-
+const [openHead, closeHead] = startingRawHTMLFragment
+    .replace(headString, `${headString}${splitter}`)
+    .split(splitter)
 
 export const getHTMLFragments = () => {
-    const startingHTMLFragment = `${startingRawHTMLFragment}`
-    return [startingHTMLFragment, endingRawHTMLFragment]
-}
+    return [openHead, closeHead, endingRawHTMLFragment]
+};
+
+export const parseHelmetTemplate = helmet => (template, ...vars) => {
+    const matcher = /{(\w*)}/g;
+
+    return (helmet && template && template.map(
+        item => item.trim()
+            .replace(matcher, (value, prop) => (helmet[prop] && helmet[prop].toString()) || "")
+    )
+        .reduce((acc, item, i) => {
+            acc.push(item);
+            vars[i] && acc.push(vars[i])
+            return acc;
+        }, [])
+        .join("").trim()) || "";
+};
--- server/lib/ssr.js
+++ server/lib/ssr.js
@@ -1,78 +1,66 @@
+import 'babel-polyfill';
+
+import path from "path";
+import log from 'llog'
+import through from 'through'
+
 import React from 'react'
 import { renderToNodeStream } from 'react-dom/server'
 import { HelmetProvider } from 'react-helmet-async'
 import { StaticRouter } from 'react-router-dom'
-import log from 'llog'
-import through from 'through'
-import 'babel-polyfill';
-import { getHTMLFragments } from './client'
-import {serialize} from "components/state-mutate-with-status/utils";
 import { ChunkExtractor } from '@loadable/server'
-import {FormDataProvider, parseFormData} from "components/actions/form";
 import {set} from "lodash";
-import path from "path";
 import { Provider } from 'react-redux';
 
+import {serialize} from "components/state-mutate-with-status/utils";
+import {FormDataProvider, parseFormData} from "components/actions/form";
 import {getDataFromTree} from "components/actions";
 import ErrorProvider from "components/actions/ErrorProvider";
 import {errorLike} from "components/error";
 import StylesheetServer from "components/myStyled/server";
 
-import getStore from "../../app/store";
-import App from '../../app/App'
+import getStore from "app/store";
+import App from 'app/App';
 
-const parseHelmetTemplate = helmet => (template, ...vars) => {
-    const matcher = /{(\w*)}/g;
-
-    return helmet && template && template.map(
-        item => item.trim()
-            .replace(matcher, (value, prop) => (helmet[prop] && helmet[prop].toString()) || "")
-    )
-        .reduce((acc, item, i) => {
-            acc.push(item);
-            vars[i] && acc.push(vars[i])
-            return acc;
-        }, [])
-        .join("") || "";
-};
+import {getHTMLFragments, parseHelmetTemplate} from './client';
 
 const statsFile = path.resolve('dist/client/loadable-stats.json');
 
 export default async (req, res) => {
-    const t1 = Date.now();
-    const store = getStore();
-
-    const context = {};
-    const helmetContext = {};
-    const errorContext = {};
-    const postData = req.method === "POST" && Object.keys(req.body).reduce((o, key) => {
-        set(o, key, req.body[key])
-        return o;
-    }, {});
-    const formData = parseFormData(req.method === "POST" && postData);
-
-    console.log("FORMDATA", formData)
-
-    const extractor = new ChunkExtractor({ statsFile });
-
-    const app = extractor.collectChunks(
-        <FormDataProvider value={formData}>
-            <ErrorProvider value={errorContext}>
-                <HelmetProvider context={helmetContext}>
-                    <Provider store={store}>
-                        <StaticRouter
-                            location={req.originalUrl}
-                            context={context}
-                        >
-                            <App/>
-                        </StaticRouter>
-                    </Provider>
-                </HelmetProvider>
-            </ErrorProvider>
-        </FormDataProvider>
-    );
-
     try {
+        const t1 = Date.now();
+        const store = getStore();
+
+        const context = {};
+        const helmetContext = {};
+        const errorContext = {};
+        const postData = req.method === "POST" && Object.keys(req.body).reduce((o, key) => {
+            set(o, key, req.body[key]);
+            return o;
+        }, {});
+        const formData = parseFormData(req.method === "POST" && postData);
+
+        console.log("FORMDATA", formData);
+
+        const extractor = new ChunkExtractor({ statsFile });
+
+        const app = extractor.collectChunks(
+            <FormDataProvider value={formData}>
+                <ErrorProvider value={errorContext}>
+                    <HelmetProvider context={helmetContext}>
+                        <Provider store={store}>
+                            <StaticRouter
+                                location={req.originalUrl}
+                                context={context}
+                            >
+                                <App/>
+                            </StaticRouter>
+                        </Provider>
+                    </HelmetProvider>
+                </ErrorProvider>
+            </FormDataProvider>
+        );
+
         const stylesheetServer = StylesheetServer()
 
         await getDataFromTree(stylesheetServer.collectStyles(app));
@@ -90,31 +78,27 @@ export default async (req, res) => {
             const styleTags = extractor.getStyleTags();
 
             const [
-                startingHTMLFragment,
+                openHead,
+                closeHead,
                 endingHTMLFragment
             ] = getHTMLFragments()
-
-            const headString = "<head>"
-            const splitter = '###SPLIT###'
-
-            const [openHead, closeHead] = startingHTMLFragment
-                .replace(headString, `${headString}${splitter}`)
-                .split(splitter)
 
             const {helmet} = helmetContext;
 
             res.setHeader('Content-Type', 'text/html; charset=utf-8');
-            res.status(200)
+            res.status(200);
+
             res.write(`
                 ${openHead}
                 ${parseHelmetTemplate(helmet)`
-                {title}{meta}
-                <link rel="canonical" href="${req.protocol}://${req.hostname}${req.originalUrl}" />
+                    {title}{meta}
+                    <link rel="canonical" href="${req.protocol}://${req.hostname}${req.originalUrl}" />
                 `}
                 ${linkTags}
                 ${styleTags}
                 ${closeHead}
-            `.trim())
+            `);
+
             stream
                 .pipe(
                     through(
@@ -122,7 +106,6 @@ export default async (req, res) => {
                             this.queue(data)
                         },
                         function end() {
-                            this.queue(scriptTags)
                             this.queue(
                                 `<script>
                                     window.__PRELOADED_STATE__ = ${serialize(store.getState()).replace(
@@ -136,6 +119,7 @@ export default async (req, res) => {
                                     window.__ERROR_STATE__ = ${JSON.stringify(errorContext && errorContext.error && errorLike(errorContext.error))}
                                 </script>`
                             )
+                            this.queue(scriptTags)
                             this.queue(endingHTMLFragment)
                             this.queue(null)
                             console.log("----DONE", Date.now() - t1)
