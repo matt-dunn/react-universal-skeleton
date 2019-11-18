73f4f24c32c48bc5e1ab5ed2ad8d7a96becabeca
Matt Dunn <matt.j.dunn@gmail.com>
2019-11-18 09:40:25 +0000

    Set focus to first form element if form level error occurs

--- components/Form/FormErrorFocus.ts
+++ components/Form/FormErrorFocus.ts
@@ -2,16 +2,17 @@ import {useFormikContext} from "formik";
 import {RefObject, useEffect} from "react";
 
 import {typedMemo} from "./types";
+import {useFormContext} from "./utils";
 
 type FormErrorFocusProps = {
     formRef: RefObject<HTMLFormElement>;
 }
 
-function setFocus(form: HTMLFormElement) {
+function setFocus(form: HTMLFormElement, forceFirst = false) {
     // Move into next tick so avoid attempting to focus on a disabled input element
     setTimeout(() => {
         if (!document.activeElement || (document.activeElement as HTMLInputElement).tabIndex < 0) {
-            const target = form.querySelector<HTMLInputElement>(".invalid");
+            const target = (!forceFirst && form.querySelector<HTMLInputElement>(".invalid")) || (forceFirst && form.querySelector<HTMLInputElement>("input:not([type='hidden']), select, textarea, [tabIndex]"));
             if (target) {
                 if (target.tabIndex >= 0) {
                     target.focus();
@@ -26,10 +27,11 @@ function setFocus(form: HTMLFormElement) {
 
 function FormErrorFocus<T>({formRef}: FormErrorFocusProps) {
     const {isSubmitting, isValidating, isValid} = useFormikContext<T>();
+    const {formData} = useFormContext<T, any, any>();
 
     useEffect(() => {
-        !isValid && formRef.current && setFocus(formRef.current);
-    }, [formRef, isValid]);
+        (!isValid || formData.error) && formRef.current && setFocus(formRef.current, Boolean(formData.error));
+    }, [formRef, isValid, formData.error]);
 
     if (isSubmitting && !isValidating) {
         formRef.current && setFocus(formRef.current);
