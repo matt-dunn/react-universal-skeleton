66df97491217841677455b0b924973202191fc9c
Matt Dunn <matt.j.dunn@gmail.com>
2019-11-26 17:26:04 +0000

    Use the processed dist html as server template

--- app/webpack/client.config.js
+++ app/webpack/client.config.js
@@ -9,6 +9,7 @@ const BrotliPlugin = require("brotli-webpack-plugin");
 const LoadablePlugin = require("@loadable/webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 // const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 const ROOT = path.join(__dirname, "../../");
 
@@ -133,8 +134,10 @@ module.exports = {
                 template: path.resolve(__dirname, "../index.html"),
                 chunksSortMode: "none",
                 //favicon: path.resolve(__dirname, "../favicon.ico")
-                build: BUILD
+                build: BUILD,
+                alwaysWriteToDisk: true
             }),
+            new HtmlWebpackHarddiskPlugin(),
             new MiniCssExtractPlugin({
                 filename: "[name]-[hash].css",
                 chunkFilename: "[id]-[hash].css",
