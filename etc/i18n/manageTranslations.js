44eb1cc39f19dedd9c080027cea55f421b30b762
Matt Dunn <matt.j.dunn@gmail.com>
2019-12-12 13:18:43 +0000

    Rework manage translations

--- etc/i18n/manageTranslations.js
+++ etc/i18n/manageTranslations.js
@@ -4,29 +4,21 @@ import stringify from "json-stable-stringify";
 import { parse } from "intl-messageformat-parser";
 import chalk from "chalk";
 import {Table} from "console-table-printer";
+import {isEqual} from "lodash";
 
 import metadata from "app/webpack/metadata";
 
-const {availableLocales, i18nMessagesPath, i18nLocalePath} = metadata();
-// const reportFilename = path.join(reportsPath, "i18l-untranslated.json");
+const {availableLocales, i18nMessagesPath, i18nLocalePath, reportsPath} = metadata();
 
-const getDefaultMessages = () => JSON.parse(fs.readFileSync(path.join(process.cwd(), i18nMessagesPath, "defaultMessages.json")).toString());
+const getDefaultMessages = (messagesPath) => JSON.parse(fs.readFileSync(path.join(process.cwd(), messagesPath, "defaultMessages.json")).toString());
 
-const getLangMessages = (lang = "default") => {
-    if (lang !== "default" && availableLocales.indexOf(lang) === -1) {
-        throw new Error(`Language '${lang} is not defined. Available: [${availableLocales}]`);
-    }
-
-    const filename = path.join(i18nLocalePath, `_${lang}.json`);
+const getLangMessages = (translationsPath, lang = "default") => {
+    const filename = path.join(translationsPath, `_${lang}.json`);
     return (fs.existsSync(filename) && JSON.parse(fs.readFileSync(filename).toString())) || [];
 };
 
-const saveLangMessages = (messages, lang = "default") => {
-    if (lang !== "default" && availableLocales.indexOf(lang) === -1) {
-        throw new Error(`Language '${lang} is not defined. Available: [${availableLocales}]`);
-    }
-
-    const filename = path.join(i18nLocalePath, `_${lang}.json`);
+const saveLangMessages = (translationsPath, messages, lang = "default") => {
+    const filename = path.join(translationsPath, `_${lang}.json`);
     fs.writeFileSync(filename, stringify(messages, {
         space: 2,
         trailingNewline: false
@@ -54,23 +46,18 @@ const formatNumber = num => num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$
 
 const convertHashToArray = messages => Object.values(messages);
 
-const defaultLangMessages = getLangMessages();
-const defaultLangMessagesHash = hashMessages(defaultLangMessages);
-const defaultMessages = getDefaultMessages();
-const defaultMessagesHash = hashMessages(defaultMessages);
-
-const translationCount = defaultLangMessages.length;
-
-const getDelta = (messages) => {
+const getDelta = (sourceDefaultMessages, defaultLangMessages, messages) => {
     const messagesHash = hashMessages(messages);
+    const defaultLangMessagesHash = hashMessages(defaultLangMessages);
+    const defaultMessagesHash = hashMessages(sourceDefaultMessages);
 
-    const delta = defaultMessages.reduce((delta, message) => {
+    const delta = sourceDefaultMessages.reduce((delta, message) => {
         const {id} = message;
 
         if (!messagesHash[id]) {
             delta.added[id] = message;
             delta.untranslated[id] = message;
-        } else if (defaultMessagesHash[id].defaultMessage !== defaultLangMessagesHash[id].defaultMessage) {
+        } else if (defaultLangMessagesHash[id] && defaultMessagesHash[id].defaultMessage !== defaultLangMessagesHash[id].defaultMessage) {
             delta.updated[id] = message;
             delta.untranslated[id] = message;
         } else if (messagesHash[id] && !defaultMessagesHash[id]) {
@@ -98,7 +85,7 @@ const getDelta = (messages) => {
     return delta;
 };
 
-const applyDelta = (lang, messages, {added, removed, updated}) => {
+const applyDelta = (messages, {added, removed, updated}) => {
     return messages
         .map(message => {
             const {id} = message;
@@ -114,42 +101,157 @@ const applyDelta = (lang, messages, {added, removed, updated}) => {
         .concat(Object.keys(added).map(key => added[key]));
 };
 
-try {
-    const t = new Table({
-        columns: [
-            { name: "Language", alignment: "left" }, //with alignment
-            { name: "Translations", alignment: "right" },
-            { name: "Outstanding", alignment: "right" },
-            { name: "% Complete", alignment: "right" },
-            { name: "≈ Outstanding Words", alignment: "right" },
-        ],
-    });
+const translations = ({messagesPath, translationsPath, reportsPath, languages}) => {
+    return {
+        manage: ({update} = {update: true}) => {
+            const report = {
+                updated: false,
+                timestamp: undefined,
+                summary: {
+                    totalLanguagesCount: languages.length,
+                    totalTranslationsCount: 0,
+                    totalUntranslatedCount: 0,
+                    totalWordCount: 0,
+                },
+                languages: []
+            };
 
-    availableLocales.forEach(lang => {
-        const messages = getLangMessages(lang);
-        const delta = getDelta(messages);
-        const updatedMessages = applyDelta(lang, messages, delta);
+            try {
+                const sourceDefaultMessages = getDefaultMessages(messagesPath);
+                const defaultLangMessages = getLangMessages(translationsPath);
 
-        // console.log(lang, delta);
+                const translationCount = sourceDefaultMessages.length;
 
-        saveLangMessages(updatedMessages, lang);
+                report.summary.totalTranslationsCount = translationCount;
 
-        const untranslatedCount = Object.keys(delta.untranslated).length;
-        const wordCount = countWords(convertHashToArray(delta.untranslated));
+                languages.forEach(lang => {
+                    const messages = getLangMessages(translationsPath, lang);
+                    const delta = getDelta(sourceDefaultMessages, defaultLangMessages, messages);
 
-        t.addRow({
-            "Language": lang,
-            "Translations": formatNumber(translationCount),
-            "Outstanding": formatNumber(untranslatedCount),
-            "% Complete": `${Math.round((100 - ((untranslatedCount / translationCount) * 100)) * 100) / 100}%`,
-            "≈ Outstanding Words": formatNumber(wordCount)
-        }, {color: untranslatedCount === 0 ? "green" : "yellow"});
-    });
+                    const untranslatedCount = Object.keys(delta.untranslated).length;
+                    const wordCount = countWords(convertHashToArray(delta.untranslated));
 
-    saveLangMessages(defaultMessages);
+                    report.summary.totalUntranslatedCount += untranslatedCount;
+                    report.summary.totalWordCount += wordCount;
 
-    t.printTable();
-} catch (ex) {
-    console.error(chalk.red(ex.message));
-    process.exit(1);
-}
+                    report.languages.push({
+                        lang,
+                        wordCount,
+                        untranslated: untranslatedCount,
+                        added: Object.keys(delta.added).length,
+                        updated: Object.keys(delta.updated).length,
+                        removed: Object.keys(delta.removed).length,
+                        delta
+                    });
+
+                    if (update) {
+                        const updatedMessages = applyDelta(messages, delta);
+                        saveLangMessages(translationsPath, updatedMessages, lang);
+                    }
+                });
+
+                update && saveLangMessages(translationsPath, sourceDefaultMessages);
+
+                report.updated = update && new Date().toISOString();
+                report.timestamp = new Date().toISOString();
+
+                const managed = {
+                    getReport: () => report,
+                    saveReport: () => {
+                        fs.writeFileSync(path.join(reportsPath, "i18l-untranslated.json"), stringify(report, {
+                            space: 2,
+                            trailingNewline: false
+                        }));
+
+                        return managed;
+                    },
+                    getSummary: () => {
+                        return {
+                            ...report.summary,
+                            translationLanguages: languages,
+                            languages: report.languages.map(language => {
+                                const {delta, ...rest} = language;  // eslint-disable-line no-unused-vars
+                                return rest;
+                            })
+                        };
+                    },
+                    updateSummary: () => {
+                        const {timestamp} = report;
+                        const summary = managed.getSummary();
+
+                        const filename = path.join(translationsPath, "summary.json");
+                        const summaryReport = (fs.existsSync(filename) && JSON.parse(fs.readFileSync(filename).toString())) || {
+                            languages: []
+                        };
+
+                        const lastSummary = summaryReport.languages[summaryReport.languages.length - 1];
+
+                        if (!lastSummary || !isEqual(lastSummary.summary, summary)) {
+                            summaryReport.languages.push({timestamp, summary});
+
+                            fs.writeFileSync(filename, stringify(summaryReport, {
+                                space: 2,
+                                trailingNewline: false
+                            }));
+                        }
+
+                        return managed;
+                    },
+                    printSummary: () => {
+                        const t = new Table({
+                            columns: [
+                                {name: "Language", alignment: "left"},
+                                {name: "Translations", alignment: "right"},
+                                {name: "Outstanding", alignment: "right"},
+                                {name: "Added", alignment: "right"},
+                                {name: "Removed", alignment: "right"},
+                                {name: "Updated", alignment: "right"},
+                                {name: "% Complete", alignment: "right"},
+                                {name: "≈ Outstanding Words", alignment: "right"},
+                            ],
+                        });
+
+                        report.languages.forEach(({lang, untranslated, wordCount, delta}) => {
+                            t.addRow({
+                                "Language": lang,
+                                "Translations": formatNumber(translationCount),
+                                "Outstanding": formatNumber(untranslated),
+                                "Added": formatNumber(Object.keys(delta.added).length),
+                                "Removed": formatNumber(Object.keys(delta.removed).length),
+                                "Updated": formatNumber(Object.keys(delta.updated).length),
+                                "% Complete": `${Math.round((100 - ((untranslated / translationCount) * 100)) * 100) / 100}%`,
+                                "≈ Outstanding Words": formatNumber(wordCount)
+                            }, {color: untranslated === 0 ? "green" : "yellow"});
+                        });
+
+                        t.printTable();
+
+                        return managed;
+                    }
+                };
+
+                return managed;
+            } catch (ex) {
+                console.error(chalk.red(ex.message));
+                process.exit(1);
+            }
+        }
+    };
+};
+
+const managedTranslations = translations({
+    messagesPath: i18nMessagesPath,
+    translationsPath: i18nLocalePath,
+    languages: availableLocales,
+    reportsPath
+}).manage({
+    update: true
+});
+
+managedTranslations
+    .printSummary()
+    .saveReport()
+    .updateSummary();
+
+// console.log(managedTranslations.getReport())
+// console.log("SUMMARY",managedTranslations.getSummary())
