a6fdde1dc6f3d9c71b8cb030e655d32208fd9423
Matt Dunn <matt.j.dunn@gmail.com>
2020-01-25 14:41:55 +0000

    POC

--- app/containers/State/TestComponent.tsx
+++ app/containers/State/TestComponent.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from "react";
+import React, {ComponentType, ReactNode} from "react";
 import styled from "@emotion/styled";
 
 import Loading from "components/Loading";
@@ -76,3 +76,64 @@ export const TestComponent = ({asyncData, syncData, getAsyncData, getSyncData, d
     );
 };
 
+
+export type GetProps<C> = C extends ComponentType<infer P> ? P : never;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type Shared<
+    InjectedProps,
+    DecorationTargetProps
+    > = {
+    [P in Extract<keyof InjectedProps, keyof DecorationTargetProps>]?: InjectedProps[P] extends DecorationTargetProps[P] ? DecorationTargetProps[P] : never;
+};
+
+
+type MapStateToProps<S, TState> = {
+    (state: TState): S
+}
+
+type MapDispatchToProps<S> = {
+    (): S
+}
+
+const xx = <C extends ComponentType<any>, O, P extends GetProps<C>, S extends Partial<GetProps<C>>, T extends Partial<GetProps<C>>, TState = {}>(
+    Component: C,
+    mapStateToProps: MapStateToProps<S, TState>,
+    mapDispatchToProps: MapDispatchToProps<T>
+): {test: {(props?: P): Omit<GetProps<C>, keyof Shared<S & T, GetProps<C>>>}} => {
+    const state: TState = {} as TState;
+
+    return {
+        test: (props) => {
+            return {
+                ...props,
+                ...mapStateToProps(state),
+                ...mapDispatchToProps()
+            }
+        }
+    }
+}
+
+const x = xx(
+    TestComponent,
+    (state) => {
+        return {
+            asyncData: {id: "2", data: "", timestamp: 234},
+            // syncData: {id: "2", data: "", timestamp: 234},
+            xxx: state
+        }
+    },
+    () => {
+        return {
+            getAsyncData: 34,
+            getSyncData: 34
+        }
+    }
+)
+
+const y = x.test()
+
+const aa1 =y.asyncData;
+const aa2 =y.syncData;
+const aa3 =y.getAsyncData;
+const aa4 =y.getSyncData;
+const aa5 =y.description;
