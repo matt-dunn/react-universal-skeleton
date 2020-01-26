6e4b59ce4e9e17dbd46a3f51cf1c9f84a94c2294
Matt Dunn <matt.j.dunn@gmail.com>
2020-01-26 17:05:20 +0000

    Updates

--- app/containers/State/simpleState/middleware/index.ts
+++ app/containers/State/simpleState/middleware/index.ts
@@ -1,64 +1,6 @@
 // - Example middleware --------------------------------------------------------------------------------------------------------------------
 
-import isPromise from "is-promise";
-
-import {Middleware} from "./exampleStateManagement";
-import {errorLike, ErrorLike} from "./utils";
-
-const $status = Symbol("$status");
-
-type Status = {
-    readonly processing: boolean;
-    readonly complete: boolean;
-    readonly error?: ErrorLike;
-}
-
-export type DecoratedWithStatus = {
-    readonly [$status]?: Status;
-}
-
-const Status = (status?: Partial<Status>): Status => ({
-    processing: false,
-    complete: false,
-    ...status
-});
-
-const decorateWithStatus = <P extends DecoratedWithStatus>(status?: Partial<Status>, payload?: P): P & DecoratedWithStatus => ({
-    ...payload || {} as P,
-    [$status]: Status(status)
-});
-
-export const getStatus = <P extends DecoratedWithStatus | undefined>(payload: P): Status => (payload && payload[$status]) || Status();
-
-export const simplePromiseDecorator: Middleware = async (action, next) => {
-    if (isPromise(action.payload)) {
-        try {
-            next({
-                ...action,
-                payload: decorateWithStatus({
-                    processing: true
-                })
-            });
-
-            next({
-                ...action,
-                payload: decorateWithStatus({
-                    complete: true
-                }, await action.payload)
-            });
-        } catch(error) {
-            next({
-                ...action,
-                payload: decorateWithStatus({
-                    error: errorLike(error)
-                }),
-                error: true
-            });
-        }
-    } else {
-        next(action);
-    }
-};
+import {Middleware} from "../exampleStateManagement";
 
 export const simpleAsyncDecorator: Middleware = (action, next) => {
     setTimeout(() => {
@@ -82,3 +24,4 @@ export const simpleDecorator: Middleware = (action, next) => {
     });
 };
 
+export * from "./simplePromiseDecorator";
