558eb1f11278d1a8d9e3798adfed220a7dba89e1
Matt Dunn <matt.j.dunn@gmail.com>
2020-01-27 15:09:47 +0000

    Add redux dev tools debug tooling

--- app/store.js
+++ app/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import stateDecorator from "components/redux/middleware/stateDecorator";
 import notification from "components/redux/middleware/notification";
@@ -8,16 +8,18 @@ import rootReducer from "./reducers";
 import servicesExample from "./components/api/__dummy__/example";
 import servicesAuth from "./components/api/auth";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const getStore = (initialState = {}) => createStore(
     rootReducer,
     initialState,
-    applyMiddleware(
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeEnhancers(applyMiddleware(
         // promiseMiddleware,
         stateDecorator({ dependencies: { services: {...servicesExample, ...servicesAuth} } }),
         notification({notify: () => import("./notify").then(module => module.notify)}),
         // thunkMiddleware,
         // loggerMiddleware,
-    ),
+    )),
 );
 
 export default getStore;
