591270b5454ed8a7a1c41c7b4ece0360ae4d26bb
Matt Dunn <matt.j.dunn@gmail.com>
2020-01-28 15:16:23 +0000

    Check window

--- app/store.js
+++ app/store.js
@@ -10,7 +10,7 @@ import rootReducer from "./reducers";
 // import servicesAuth from "./components/api/auth";
 import {asyncAction} from "./saga";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const sagaMiddleware = createSagaMiddleware();
 
