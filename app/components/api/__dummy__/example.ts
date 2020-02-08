c902b298ca21d57cae26d7fb261cae9956c33b23
Matt Dunn <matt.j.dunn@gmail.com>
2020-02-08 13:30:41 +0000

    Do not throw error

--- app/components/api/__dummy__/example.ts
+++ app/components/api/__dummy__/example.ts
@@ -44,7 +44,7 @@ export const ExampleApi: ExampleApi = {
             if (!isAuthenticated()) {
                 throw new APIError("Auth Error...", 123, 401);
             }
-            throw new APPError("Error in exampleGetList", 123);
+            // throw new APPError("Error in exampleGetList", 123);
         }
 
         // throw new APIError("Auth Error...", 123, 401)
