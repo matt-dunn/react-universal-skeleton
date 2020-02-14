b05f34e033fe66fc650deb75cfbcfaaaccd56bb5
Matt Dunn <matt.j.dunn@gmail.com>
2020-02-14 16:47:11 +0000

    Updates

--- app/styles/Components.jsx
+++ app/styles/Components.jsx
@@ -1,8 +1,11 @@
 import styled from "@emotion/styled";
 
-const Page = styled.main`
+export const Main = styled.main`
   max-width: 100vw;
   padding: 20px;
 `;
 
-export default Page;
+export const Title = styled.h1`
+    color: #ccc;
+    margin-bottom: 20px;
+`;
