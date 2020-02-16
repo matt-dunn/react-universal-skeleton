0db4d1d12f90bc42188e9ba1d610a8a114ca839a
Matt Dunn <matt.j.dunn@gmail.com>
2020-02-16 11:12:17 +0000

    Update types

--- components/api/types.ts
+++ components/api/types.ts
@@ -1,31 +1,3 @@
-// type Callback = {
-//     (): void;
-// }
-//
-// export type Cancel = (cb: Callback) => Callback
-//
-// type Cancelable = {
-//     canceller: Cancel;
-//     cancel: () => void;
-// }
-//
-// export const Cancellable = function(): Cancelable {
-//     let callback: Callback;
-//     let isCancelled = false;
-//
-//     return {
-//         canceller: (cb: Callback) => {
-//             isCancelled && cb();
-//             return callback = cb;
-//         },
-//         cancel: () => {
-//             isCancelled = true;
-//             callback && callback();
-//         }
-//     };
-// };
-//
-export type WrapCancelable<T extends (...args: any[]) => any> = {
+export type WrapWithAbortSignal<T extends (...args: any[]) => any> = {
     (...args: Parameters<T>): (signal?: AbortSignal) => ReturnType<T>;
 }
-//
